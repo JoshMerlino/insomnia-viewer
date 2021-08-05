d94de91a9ef0fcf391336dd91e93631077bab838
Josh Merlino <joshua.merlino@gmail.com>
2021-08-05 09:14:18 -0400

    update photoncss

--- webpack.conf.ts
+++ webpack.conf.ts
@@ -62,6 +62,7 @@ export = <Configuration>{
 				}
 			} ]
 		}, {
+			test: /\.([\s\S]*?)(woff|woff2|eot|ttf|otf)$/,
 			include: path.resolve("web/static"),
 			use: [ {
 				loader: "file-loader",

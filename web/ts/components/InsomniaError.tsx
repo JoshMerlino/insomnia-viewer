561f5b3a1593cacb15bbdffcab49755d849ac5c5
Josh Merlino <joshua.merlino@gmail.com>
2021-09-23 14:23:46 -0400

    update insomniaError

--- web/ts/components/InsomniaError.tsx
+++ web/ts/components/InsomniaError.tsx
@@ -8,7 +8,7 @@ export default function InsomniaError(): JSX.Element | null {
 			<Container>
 				<TextIcon>warning</TextIcon>
 				<h1>Documentation not found!</h1>
-				<p>It looks like it&apos;s not possible to retrieve the contents of the API documentation at the moment. If you&apos;re the owner of this site, make sure that your <a href="/insomnia.json">Insomnia JSON file</a> is accessible.</p>
+				<p>It looks like it's not possible to retrieve the contents of the API documentation at the moment. If you're the owner of this site, make sure that your <a href="/insomnia.json">Insomnia JSON file</a> is accessible.</p>
 				<p>The developer console of your browser (network tab) might have more things to say about this error.</p>
 			</Container>
 		</div>
